002cc5c288a128532256d2c903bc992ca9fc4f3b
Christian Rehn <mail@christian-rehn.de>
2018-11-25 22:34:11 +0100

    exceptions for actions and questions

--- design-cards/buildIntermediate.rb
+++ design-cards/buildIntermediate.rb
@@ -3,6 +3,55 @@
 require 'nokogiri'
 require 'json'
 
+class Card
+    attr_accessor :abbreviation, :name, :short, :long, :links, :set, :shield
+
+    def initialize(params, config)
+        params.each { |k,v| public_send("#{k}=",v) }
+        @config = config
+    end
+
+    def title
+        if symbolInConfig.nil? then
+            return "#{@abbreviation}: #{@name}"
+        else
+            return @name
+        end
+    end
+
+    def short 
+        return "»#{@short}«"
+    end
+
+    def symbol
+        if symbolInConfig.nil? then
+            return @abbreviation
+        else
+            return symbolInConfig
+        end
+    end
+
+    def symbolInConfig
+        return @config['colors'][@shield]['symbol']
+    end
+
+    def font
+        if symbolInConfig.nil?
+            return 'Metamorphous'
+        else
+            return 'Liberation Serif'
+        end
+    end
+
+    def fontSize
+        if symbolInConfig.nil?
+            return '14.81294632px'
+        else
+            return '24.1'
+        end
+    end
+end
+
 def readJson
     file = open('src/cards.json')
     json = file.read
@@ -13,16 +62,17 @@ def createCardSvg(card, colors)
     doc = File.open("src/card.svg") { |f| Nokogiri::XML(f) }
     lib = File.open("src/lib.svg") { |f| Nokogiri::XML(f) }
 
-    setSingleLineText doc, 'abbreviation', card["abbreviation"]
-    setSingleLineText doc, 'title', "#{card["abbreviation"]}: #{card["name"]}"
-    setMultiLineText doc, 'short', "»#{card["short"]}«"
-    setMultiLineText doc, 'long', card["long"]
-    setSingleLineText doc, 'links', card["links"]
-    setCardSet doc, lib, card['set']
-    setShield doc, lib, card['shield']
+    setSingleLineText doc, 'abbreviation', card.symbol
+    setSingleLineText doc, 'title', card.title
+    setMultiLineText doc, 'short', card.short
+    setMultiLineText doc, 'long', card.long
+    setSingleLineText doc, 'links', card.links
+    setCardSet doc, lib, card.set
+    setShield doc, lib, card.shield
     adjustColors doc, card, colors
+    setFont doc, 'abbreviation', card.font, card.fontSize
 
-    File.write("intermediate/#{card["abbreviation"]}.svg", doc) 
+    File.write("intermediate/#{card.abbreviation}.svg", doc) 
 end
 
 def setSingleLineText(doc, id, value)
@@ -50,14 +100,14 @@ def setShield(doc, lib, value)
 end
 
 def adjustColors(doc, card, colors)
-    replaceColors doc, colors['simple']['background'], colors[card['shield']]['background'] 
-    replaceColors doc, colors['simple']['line'], colors[card['shield']]['line'] 
+    replaceColors doc, colors['simple']['background'], colors[card.shield]['background'] 
+    replaceColors doc, colors['simple']['line'], colors[card.shield]['line'] 
 
     title = doc.at_xpath("//*[@id='text_title']/svg:tspan") 
-    title['style'] = title['style'].gsub /#ffffff/, colors[card['shield']]['title']
+    title['style'] = title['style'].gsub /#ffffff/, colors[card.shield]['title']
 
     marbleCircle = doc.at_xpath("//*[@id='card_set']//svg:circle[@transform]") 
-    marbleCircle['style'] = marbleCircle['style'].gsub /fill:#[0-9a-f]+/, "fill:#{colors[card['shield']]['marble']}"
+    marbleCircle['style'] = marbleCircle['style'].gsub /fill:#[0-9a-f]+/, "fill:#{colors[card.shield]['marble']}"
 end
 
 def replaceColors(doc, srcColor, replacementColor)
@@ -67,7 +117,15 @@ def replaceColors(doc, srcColor, replacementColor)
     end
 end
 
+def setFont(doc, id, font, size)
+    tspan = doc.at_xpath("//svg:tspan[@id='#{id}']")
+    tspan['style'] = tspan['style'].gsub /font-family:'?[^';]*'?/, "font-family:'#{font}'"
+    tspan['style'] = tspan['style'].gsub /-inkscape-font-specification:'?[^';]*'?/, "-inkscape-font-specification:'#{font}'"
+    tspan['style'] = tspan['style'].gsub /font-size:[^';]*/, "font-size:#{size}"
+end
+
 config = readJson()
 config["cards"].each do |card|
-    createCardSvg card, config['colors']
+    createCardSvg Card.new(card, config), config['colors']
 end
+
