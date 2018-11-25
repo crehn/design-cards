7db300945465669e674b2ae603c98b4a5bf045ad
Christian Rehn <mail@christian-rehn.de>
2018-11-25 18:30:03 +0100

    WIP automation: card set and shield

--- design-cards/buildIntermediate.rb
+++ design-cards/buildIntermediate.rb
@@ -11,12 +11,15 @@ end
 
 def createCardSvg(card)
     doc = File.open("src/card.svg") { |f| Nokogiri::XML(f) }
+    lib = File.open("src/lib.svg") { |f| Nokogiri::XML(f) }
 
     setSingleLineText doc, 'abbreviation', card["abbreviation"]
     setSingleLineText doc, 'title', "#{card["abbreviation"]}: #{card["name"]}"
-    setMultiLineText doc, 'short', card["short"]
+    setMultiLineText doc, 'short', "»#{card["short"]}«"
     setMultiLineText doc, 'long', card["long"]
     setSingleLineText doc, 'links', card["links"]
+    setCardSet doc, lib, card['set']
+    setShield doc, lib, card['shield']
 
     File.write("intermediate/#{card["abbreviation"]}.svg", doc) 
 end
@@ -29,6 +32,20 @@ end
 def setMultiLineText(doc, id, value)
     tspan = doc.at_xpath("//svg:flowPara[@id='#{id}']")
     tspan.content = value
+end
+
+def setCardSet(doc, lib, set)
+    card_set = doc.at_xpath("//svg:g[@id='card_set']") 
+    card_set.children.remove
+    set = lib.at_xpath("//svg:g[@id='#{set}_set']")
+    card_set.add_child(set.children)
+end
+
+def setShield(doc, lib, value)
+    shield = doc.at_xpath("//svg:g[@id='shield']") 
+    shield.children.remove
+    newShield = lib.at_xpath("//svg:g[@id='#{value}']")
+    shield.add_child(newShield.children)
 end
 
 cards = readJson()["cards"]
--- design-cards/buildOut.sh
+++ design-cards/buildOut.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 for i in intermediate/*.svg ; do
-    inkscape -d 600 -e out/`basename $i .svg`.png $i
+    inkscape -d 300 -e out/`basename $i .svg`.png $i
 done
